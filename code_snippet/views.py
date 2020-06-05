2d3b3605d03f34e606c3a151e89a2f6e5f808ae5
AaronOBrien1453 <aatobrien@gmail.com>
2020-06-05 13:53:21 -0400

    fixed spelling error that was causing homepage not to show up

--- code_snippet/views.py
+++ code_snippet/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "code_snippet/home.html")
+    return render(request, "code_snippet/home.html", {"test": True})
