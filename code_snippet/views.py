4c8cfd90f0dad5e4fa401194e6093106639391a6
AaronOBrien1453 <aatobrien@gmail.com>
2020-06-07 16:19:49 -0400

    snippets are showing up

--- code_snippet/views.py
+++ code_snippet/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from users.models import User
 from .models import CodeSnippet
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def homepage(request):
+    if request.user.is_authenticated:
+        return redirect(to='snippet_list')
     return render(request, "code_snippet/home.html")
 
+
 def snippet_list(request):
-    code_snippet = request.user.snippet_list.all()
+    code_snippet = request.user.code_snippets.all()
     return render(request, "code_snippet/snippet_list.html", {"code_snippet": code_snippet})
